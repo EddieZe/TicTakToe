c9af17b71464b58909181d88729f7a6e1d67d84a
TripDox <eddie.zeltser@gmail.com>
2018-10-26 21:43:36 +0300

    upgrade to using array of winning sets

--- src/components/Board.js
+++ src/components/Board.js
@@ -14,7 +14,17 @@ class Board extends Component {
             boardSize: 3,
             board: [],
             onTurnTaken: props.onTurnTaken,
-            nextTurn: props.firstTurn
+            nextTurn: props.firstTurn,
+            winningSets : [
+                [[0, 0], [0, 1], [0, 2]],
+                [[1, 0], [1, 1], [1, 2]],
+                [[2, 0], [2, 1], [2, 2]],
+                [[0, 0], [1, 0], [2, 0]],
+                [[0, 1], [1, 1], [2, 1]],
+                [[0, 2], [1, 2], [2, 2]],
+                [[0, 0], [1, 1], [2, 2]],
+                [[0, 2], [1, 1], [2, 0]]
+            ]
         };
         let board = this.state.board;
         for (var i = 0; i < this.state.boardSize; i++)
@@ -25,43 +35,24 @@ class Board extends Component {
     }
 
     whoHasWon() {
+        const sets = this.state.winningSets;
         let board = this.state.board;
-        if ('1' !== board[0][0] && (board[0][0] === board[0][1] && board[0][1] === board[0][2])) {
-            return board[0][0]
+        for (let i = 0; i < sets.length; i++) {
+            const [a,b,c] = sets[i];
+            if (board[a[0]][a[1]] && board[a[0]][a[1]] === board[b[0]][b[1]] && board[a[0]][a[1]] === board[c[0]][c[1]]) {
+                return board[a[0]][a[1]]
+            }
         }
-        else if ('1' !== board[1][0] && (board[1][0] === board[1][1] && board[1][1] === board[1][2])) {
-            return board[1][0]
-        }
-        else if ('1' !== board[2][0] && (board[2][0] === board[2][1] && board[2][1] === board[2][2])) {
-            return board[2][0]
-        }
-
-        else if ('1' !== board[0][0] && (board[0][0] === board[1][0] && board[1][0] === board[2][0])) {
-            return board[0][0]
-        }
-        else if ('1' !== board[0][1] && (board[0][1] === board[1][1] && board[1][1] === board[2][1])) {
-            return board[0][1]
-        }
-        else if ('1' !== board[0][2] && (board[0][2] === board[1][2] && board[1][2] === board[2][2])) {
-            return board[0][2]
-        }
-
-        else if ('1' !== board[0][0] && (board[0][0] === board[1][1] && board[1][1] === board[2][2])) {
-            return board[0][0]
-        }
-        else if ('1' !== board[2][0] && (board[2][0] === board[1][1] && board[1][1] === board[0][2])) {
-            return board[2][0]
-        }
-        else {
-            return false;
-        }
+        return false;
     }
 
     handleClick(x, y) {
         let b = this.state.board;
         b[x][y] = this.state.nextTurn;
         let winner = this.whoHasWon();
-        if (winner){console.log(winner + ' has Won')}
+        if (winner) {
+            console.log(winner + ' has Won')
+        }
         this.setState({board: b, nextTurn: this.state.onTurnTaken()});
     }
 

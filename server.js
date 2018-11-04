daed2abdeaf0bf901aeb289bb19c32416b40f1ea
TripDox <eddie.zeltser@gmail.com>
2018-11-04 12:30:57 +0200

    WIP version to fix issue with init and final win

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
  */
 
 const io = require('socket.io')();
-const maxNumOfPlayers = 2, boardSize = 3;
+const maxNumOfPlayers = 1, boardSize = 3;
 const winningSets = [
     [[0, 0], [0, 1], [0, 2]],
     [[1, 0], [1, 1], [1, 2]],
@@ -16,6 +16,8 @@ const winningSets = [
     [[0, 0], [1, 1], [2, 2]],
     [[0, 2], [1, 1], [2, 0]]
 ];
+let board = [];
+let aiBoard = [[3, 2, 3], [2, 4, 2], [3, 2, 3]];
 const players = [];
 let gameStatus;
 
@@ -45,14 +47,24 @@ io.on('connection', (player) => {
     player.on('makeMove', (cords, sign) => {
         console.log(sign + ' asked to make the following move: ' + cords[0] + ',' + cords[1]);
         gameStatus.board[cords[0]][cords[1]] = sign;
-        gameStatus.currentTurn = gameStatus.currentTurn === 'X' ? 'O' : 'X';
-        gameStatus.moves = 9 - gameStatus.moves > 0 ? gameStatus.moves + 1 : gameStatus.moves;
+        //gameStatus.currentTurn = gameStatus.currentTurn === 'X' ? 'O' : 'X';
+        gameStatus.moves = 9 - gameStatus.moves > 0 ? gameStatus.moves + 2 : gameStatus.moves;
+
+        let nextMachineMove = blockNextWin();
+        if (nextMachineMove) {
+            aiBoard[nextMachineMove[0]][nextMachineMove[1]] = 0;
+            gameStatus.board[nextMachineMove[0]][nextMachineMove[1]] = 'O';
+        }
+        else {
+            nextMachineMove = updateAndFindBestOption(cords);
+            gameStatus.board[nextMachineMove.x][nextMachineMove.y] = 'O';
+        }
         let winner = checkIsGameFinished();
         if (winner || gameStatus.moves === 9) {
             gameStatus.winner = winner;
             gameStatus.isGameFinished = true;
         }
-        io.sockets.emit('updateStatus', gameStatus)
+        io.sockets.emit('updateStatus', gameStatus);
     });
 
     player.on('disconnect', () => {
@@ -64,6 +76,40 @@ io.on('connection', (player) => {
         }
     })
 });
+
+function updateAndFindBestOption(cords) {
+    aiBoard[cords[0]][cords[1]] = 0;
+    let max = 0, bestCell = {};
+    aiBoard.forEach((row, indexX) => {
+        row.forEach((el, indexY) => {
+            if (el > max) {
+                max = el;
+                bestCell.x = indexX;
+                bestCell.y = indexY;
+            }
+        })
+    });
+    aiBoard[bestCell.x][bestCell.y] = 0;
+    return bestCell
+}
+
+function blockNextWin() {
+    let res = null;
+    winningSets.forEach(set => {
+        let board = gameStatus.board;
+        let cellA = set[0], cellB = set[1], cellC = set[2];
+        if (board[cellA[0]][cellA[1]] === 'X' && board[cellA[0]][cellA[1]] === board[cellB[0]][cellB[1]] && !board[cellC[0]][cellC[1]]) {
+            res = cellC
+        }
+        else if (board[cellA[0]][cellA[1]] === 'X' && board[cellA[0]][cellA[1]] === board[cellC[0]][cellC[1]] && !board[cellB[0]][cellB[1]]) {
+            res = cellB
+        }
+        else if (board[cellB[0]][cellB[1]] === 'X' && board[cellB[0]][cellB[1]] === 'X' === board[cellC[0]][cellC[1]] && !board[cellA[0]][cellA[1]]) {
+            res = cellA
+        }
+    });
+    return res;
+}
 
 function checkIsGameFinished() {
     const sets = winningSets;
@@ -78,10 +124,10 @@ function checkIsGameFinished() {
 }
 
 function initGame() {
-    let board = [];
-    for (var i = 0; i < boardSize; i++) {
+    for (let i = 0; i < boardSize; i++) {
         board[i] = new Array(boardSize).fill(null);
     }
+    aiBoard = [[3, 2, 3], [2, 4, 2], [3, 2, 3]];
     gameStatus = {
         board: board,
         currentTurn: 'X',
--- src/components/Game.js
+++ src/components/Game.js
@@ -18,17 +18,7 @@ class Game extends Component {
             boardSize: 3,
             board: [],
             isGameFinished: false,
-            winner: null,
-            winningSets: [
-                [[0, 0], [0, 1], [0, 2]],
-                [[1, 0], [1, 1], [1, 2]],
-                [[2, 0], [2, 1], [2, 2]],
-                [[0, 0], [1, 0], [2, 0]],
-                [[0, 1], [1, 1], [2, 1]],
-                [[0, 2], [1, 2], [2, 2]],
-                [[0, 0], [1, 1], [2, 2]],
-                [[0, 2], [1, 1], [2, 0]]
-            ]
+            winner: null
         };
 
         this.handleClick = this.handleClick.bind(this);
